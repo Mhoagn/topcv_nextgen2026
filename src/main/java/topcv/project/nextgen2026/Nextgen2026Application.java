dba93f5aac228b8fd61ad43d76d8447986e911a2
MinhHoang <minhhoangdayyyy07@gmail.com>
2026-04-29 20:52:48 +0700

    first commit

---
+++ src/main/java/topcv/project/nextgen2026/Nextgen2026Application.java
@@ -0,0 +1,13 @@
+package topcv.project.nextgen2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nextgen2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nextgen2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/topcv/project/nextgen2026/Nextgen2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package topcv.project.nextgen2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nextgen2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
